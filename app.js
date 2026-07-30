87fa709c7242f0b042c27e720a67952c5f23d83d
Lindsey Howard <codelikeagirl91@gmail.com>
2026-07-30 17:02:51 -0500

    initial upload

---
+++ app.js
@@ -0,0 +1,60 @@
+/////////////////////////////////////////////
+// Import Our Dependencies
+/////////////////////////////////////////////
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const authRoutes = require("./routes/authRoutes.js");
+const cookieParser = require("cookie-parser");
+const { requireAuth, checkUser } = require("./middleware/auth.js");
+
+const app = express();
+
+/////////////////////////////////////////////////////
+// Middleware
+/////////////////////////////////////////////////////
+app.use(morgan("tiny")); //logging
+app.use(methodOverride("_method")); // override for put and delete requests from forms
+app.use(express.urlencoded({ extended: true })); // parse urlencoded request bodies
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set("view engine", "ejs");
+
+/////////////////////////////////////////////
+// Database Connection
+/////////////////////////////////////////////
+const dbURI = process.env.MONGODB_URI;
+const CONFIG = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+// Establish Connection
+mongoose
+  .connect(dbURI, CONFIG)
+  .catch((err) => console.log("Mongo connection failed:", err.message));
+// Events for when connection opens/disconnects/errors
+mongoose.connection
+  .on("open", () => console.log("Connected to Mongoose"))
+  .on("close", () => console.log("Disconnected from Mongoose"))
+  .on("error", (error) => console.log(error));
+
+//////////////////////////////////////////////
+// Routes
+//////////////////////////////////////////////
+app.get("*", checkUser); //apply check user middleware to every single get request and shows user in the header
+app.use(authRoutes);
+app.get("/", (req, res) => res.render("home"));
+app.get("/milkshakes", requireAuth, (req, res) => res.render("milkshakes"));
+
+//////////////////////////////////////////////
+// Server Listener
+//////////////////////////////////////////////
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Now Listening on port ${PORT}`));
